fe3c61c7627446ce81364de95301254aa0869b9f
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-02-05 20:43:26 +0100

    make get_teams_by_competition_id take string ref

--- src/db/operations_teams.rs
+++ src/db/operations_teams.rs
@@ -1,17 +1,14 @@
-use diesel::result::Error;
-use diesel::{prelude::*, insert_into};
-use crate::db::schema::teams::dsl::*;
-use crate::models::user::User;
-use crate::models::team::{SqlTeam, Team, NewTeam, BotSelector};
 use super::operations_db::establish_connection;
+use crate::db::schema::teams::dsl::*;
+use crate::models::team::{BotSelector, NewTeam, SqlTeam, Team};
+use crate::models::user::User;
+use diesel::result::Error;
+use diesel::{insert_into, prelude::*};
 
-
-pub fn create_team(team: NewTeam) ->  Result<Team, Error> {
+pub fn create_team(team: NewTeam) -> Result<Team, Error> {
     let new_team = SqlTeam::from(team);
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    let _ = insert_into(teams)
-        .values(&new_team)
-        .execute(&mut conn)?;
+    let _ = insert_into(teams).values(&new_team).execute(&mut conn)?;
     Ok(Team::from(new_team))
 }
 
@@ -25,11 +22,9 @@ pub fn join_team(team: Team, user: User) -> Result<(), Error> {
 
 pub fn get_team_by_id(uid: String) -> Result<Team, Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    match teams
-        .filter(id.eq(uid))
-        .first::<SqlTeam>(&mut conn) {
-            Ok(t) => Ok(Team::from(t)),
-            Err(e) => Err(e)
+    match teams.filter(id.eq(uid)).first::<SqlTeam>(&mut conn) {
+        Ok(t) => Ok(Team::from(t)),
+        Err(e) => Err(e),
     }
 }
 
@@ -38,9 +33,10 @@ pub fn get_team_by_student_for_competition(user: User, comp_id: String) -> Resul
     match teams
         .filter(competition_id.eq(comp_id))
         .filter(owner.eq(user.id.clone()).or(partner.eq(user.id.clone())))
-        .first::<SqlTeam>(&mut conn) {
-            Ok(t) => Ok(Team::from(t)),
-            Err(e) => Err(e)
+        .first::<SqlTeam>(&mut conn)
+    {
+        Ok(t) => Ok(Team::from(t)),
+        Err(e) => Err(e),
     }
 }
 
@@ -48,27 +44,27 @@ pub fn get_team_by_student(user: User) -> Result<Vec<Team>, Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
     match teams
         .filter(owner.eq(user.id.clone()).or(partner.eq(user.id.clone())))
-        .load::<SqlTeam>(&mut conn) {
-            Ok(t) => Ok(t.into_iter().map(Team::from).collect::<Vec<Team>>()),
-            Err(e) => Err(e)
+        .load::<SqlTeam>(&mut conn)
+    {
+        Ok(t) => Ok(t.into_iter().map(Team::from).collect::<Vec<Team>>()),
+        Err(e) => Err(e),
     }
 }
 
-pub fn get_teams_by_competition_id(com_id: String) -> Result<Vec<Team>, Error> {
+pub fn get_teams_by_competition_id(com_id: &String) -> Result<Vec<Team>, Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
     match teams
         .filter(competition_id.eq(com_id))
-        .load::<SqlTeam>(&mut conn) {
-            Ok(t) => Ok(t.into_iter().map(Team::from).collect::<Vec<Team>>()),
-            Err(e) => Err(e)
+        .load::<SqlTeam>(&mut conn)
+    {
+        Ok(t) => Ok(t.into_iter().map(Team::from).collect::<Vec<Team>>()),
+        Err(e) => Err(e),
     }
 }
 
 pub fn leave_team(team: Team, user: User) -> Result<(), Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    diesel::update(teams.filter(
-            id.eq(team.id).and(partner.eq(user.id))
-        ))
+    diesel::update(teams.filter(id.eq(team.id).and(partner.eq(user.id))))
         .set(partner.eq(""))
         .execute(&mut conn)?;
     Ok(())
@@ -76,21 +72,15 @@ pub fn leave_team(team: Team, user: User) -> Result<(), Error> {
 
 pub fn kick_partner(team: Team, user: User) -> Result<(), Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    diesel::update(teams.filter(
-            id.eq(team.id).and(owner.eq(user.id))
-        ))
+    diesel::update(teams.filter(id.eq(team.id).and(owner.eq(user.id))))
         .set(partner.eq(""))
         .execute(&mut conn)?;
     Ok(())
 }
 
-
 pub fn disband_team(team: Team, user: User) -> Result<(), Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    diesel::delete(teams.filter(
-            id.eq(team.id).and(owner.eq(user.id))
-        ))
-        .execute(&mut conn)?;
+    diesel::delete(teams.filter(id.eq(team.id).and(owner.eq(user.id)))).execute(&mut conn)?;
     Ok(())
 }
 
@@ -98,9 +88,10 @@ pub fn is_member_of_a_team(user: User) -> bool {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
     match teams
         .filter(owner.eq(user.id.clone()).or(partner.eq(user.id.clone())))
-        .first::<SqlTeam>(&mut conn) {
-            Ok(_) => true,
-            Err(_) => false
+        .first::<SqlTeam>(&mut conn)
+    {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
 
@@ -109,9 +100,10 @@ pub fn is_member_of_a_team_on_competition(user: User, comp_id: String) -> bool {
     match teams
         .filter(competition_id.eq(comp_id))
         .filter(owner.eq(user.id.clone()).or(partner.eq(user.id.clone())))
-        .first::<SqlTeam>(&mut conn) {
-            Ok(_) => true,
-            Err(_) => false
+        .first::<SqlTeam>(&mut conn)
+    {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
 
@@ -135,9 +127,7 @@ pub fn set_team_bot(team: &Team, bot: BotSelector, bot_id: String) -> Result<(),
 
 pub fn add_elo_change(tid: String, elo_change: i32) -> Result<(), Error> {
     let mut conn = establish_connection().expect("Failed to get a DB connection from the pool");
-    let team: SqlTeam = teams
-        .find(tid.clone())
-        .first(&mut conn)?;
+    let team: SqlTeam = teams.find(tid.clone()).first(&mut conn)?;
     // Calculate the new ELO
     let new_elo = team.elo + elo_change;
     // Update the team's ELO in the database
--- src/routes/competition_team_count.rs
+++ src/routes/competition_team_count.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 
-use actix_web::{HttpResponse, get};
 use crate::db::operations_competition::get_running_competitions;
 use crate::db::operations_teams::get_teams_by_competition_id;
+use actix_web::{get, HttpResponse};
 
 #[get("/competition/team/count")]
 pub async fn competition_team_count() -> HttpResponse {
     let competitions = match get_running_competitions() {
         Ok(competitions) => competitions,
-        Err(e) => return HttpResponse::InternalServerError().json(e.to_string())
+        Err(e) => return HttpResponse::InternalServerError().json(e.to_string()),
     };
 
     let mut hm: HashMap<String, usize> = HashMap::new();
 
     for competition in competitions.into_iter() {
         let id = competition.id.clone();
-        let teams = match get_teams_by_competition_id(id) {
+        let teams = match get_teams_by_competition_id(&id) {
             Ok(v) => v,
             Err(_) => continue,
         };
