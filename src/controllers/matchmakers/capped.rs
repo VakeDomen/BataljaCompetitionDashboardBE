09f45d017d7a576d2a38354b454f3cf21613ad10
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-02-05 22:40:45 +0100

    add initial runner for new game

--- src/controllers/matchmakers/capped.rs
+++ src/controllers/matchmakers/capped.rs
@@ -1,7 +1,12 @@
 use crate::{
+    controllers::{
+        elo::update_team_elo,
+        matchmakers::{classic2v2::compile_team_bots, pairmakers::random},
+    },
     db::{
-        operations_competition::get_competition_by_id,
+        operations_competition::{get_competition_by_id, set_competition_round},
         operations_teams::{get_team_by_id, get_teams_by_competition_id},
+        schema::competitions::games_per_round,
     },
     models::errors::MatchMakerError,
 };
@@ -19,5 +24,25 @@ pub fn run_round(competition_id: String) -> Result<(), MatchMakerError> {
         Err(e) => return Err(MatchMakerError::DatabaseError(e)),
     };
 
+    let compiled_teams = compile_team_bots(teams);
+    let match_pairs = random::create_match_pairs(competition.games_per_round, compiled_teams);
+
+    for (team1, team2) in match_pairs {
+        match run_match(&competition, &team1, &team2) {
+            Ok(g) => {}
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+
+    if let Err(e) = update_team_elo(match_pairs) {
+        return Err(MatchMakerError::DatabaseError(e));
+    }
+
+    let new_round = competition.round + 1;
+    if let Err(e) = set_competition_round(&competition.id, new_round) {
+        return Err(MatchMakerError::DatabaseError(e));
+    }
+
+    println!("Competition done!");
     Ok(())
 }
